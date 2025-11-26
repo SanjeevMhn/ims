d6548836147b30a4a077f03d1c148bd86d07c4c4
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-11-26 18:26:33 +0545

    [ADDED]: added grid

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,240 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, combineLatest, filter, map, startWith, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, AsyncPipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ims');
+
+  currentMonthForm = new FormGroup({
+    month: new FormControl(''),
+  });
+
+  currentMonthFormatted = new Date().getFullYear() + '-' + new Date().getMonth();
+  currentMonth = new BehaviorSubject<string>(this.currentMonthFormatted.toString());
+  selectedWeek$ = new BehaviorSubject<number | null>(null);
+  totalWeeks = 0;
+
+  getWeeks$ = combineLatest([this.currentMonth, this.selectedWeek$]).pipe(
+    map(([date, week]) => {
+      let [year, month] = date.split('-');
+      let formatted = String(year + '-' + (Number(month) + 1).toString().padStart(2, '0'));
+      this.currentMonthForm.get('month')?.setValue(formatted);
+      let firstDay = new Date(Number(year), Number(month), 1).getDay();
+      let lastDay = new Date(Number(year), Number(month) + 1, 0).getDate();
+      this.totalWeeks = Math.ceil((firstDay + lastDay) / 7);
+      return {
+        year: year,
+        month: month,
+        lastDay: lastDay,
+        week: week,
+      };
+    }),
+    map((data) => {
+      //current month's days
+      let days = [];
+      for (let i = 1; i <= data.lastDay; i++) {
+        let date: Date = new Date(Number(data.year), Number(data.month), i);
+        days.push({
+          day: date.getDay(),
+          dayIndex: date.getDate(),
+          dayName: date.toLocaleDateString('en-US', { weekday: 'short' }),
+          fullDate:
+            data.year +
+            '-' +
+            (Number(data.month) + 1).toString().padStart(2, '0') +
+            '-' +
+            date.getDate().toString().padStart(2, '0'),
+          is_current: true,
+        });
+      }
+
+      if (data.week !== null) {
+        let firstDay = days[0].day;
+        let prevMonthYear = Number(data.month) !== 0 ? Number(data.year) : Number(data.year) - 1;
+        let prevMonth = Number(data.month) !== 0 ? Number(data.month) - 1 : 11;
+        let prevMonthLastDay = new Date(prevMonthYear, prevMonth + 1, 0).getDate();
+        let prevMonthDays = [];
+        for (let i = 1; i <= prevMonthLastDay; i++) {
+          let date: Date = new Date(prevMonthYear, prevMonth, i);
+          prevMonthDays.push({
+            day: date.getDay(),
+            dayIndex: date.getDate(),
+            dayName: date.toLocaleDateString('en-US', { weekday: 'short' }),
+            fullDate:
+              prevMonthYear +
+              '-' +
+              (prevMonth + 1).toString().padStart(2, '0') +
+              '-' +
+              date.getDate().toString().padStart(2, '0'),
+            is_current: false,
+          });
+        }
+
+        let addToNewMonth = prevMonthDays.slice(-firstDay);
+        days = [...addToNewMonth, ...days];
+        let start = (Number(data.week) - 1) * 7;
+        let end = start + 7;
+        let weekDays = days.slice(start, end);
+
+        if (weekDays.length < 7) {
+          let nextMonthYear = Number(data.month) !== 11 ? Number(data.year) : Number(data.year) + 1;
+          let nextMonth = Number(data.month) !== 11 ? Number(data.month) + 1 : 0;
+          let nextMonthLastDay = new Date(nextMonthYear, nextMonth + 1, 0).getDate();
+          let nextMonthDays = [];
+
+          for (let i = 1; i <= nextMonthLastDay; i++) {
+            let date = new Date(nextMonthYear, nextMonth, i);
+            nextMonthDays.push({
+              day: date.getDay(),
+              dayIndex: date.getDate(),
+              dayName: date.toLocaleDateString('en-US', { weekday: 'short' }),
+              fullDate:
+                nextMonthYear +
+                '-' +
+                (nextMonth + 1).toString().padStart(2, '0') +
+                '-' +
+                date.getDate().toString().padStart(2, '0'),
+              is_current: false,
+            });
+          }
+
+          let sliceNumber = 7 - weekDays.length;
+          let addToLastWeek = nextMonthDays.slice(0, sliceNumber);
+          weekDays = [...weekDays, ...addToLastWeek];
+        }
+        return weekDays;
+      }
+      return days;
+    })
+  );
+
+  supplierDelivery = [
+    {
+      id: 1,
+      supplier_name: 'Ram Sharma',
+      supplier_products: [
+        {
+          id: 1,
+          product_name: 'Tea',
+        },
+        {
+          id: 2,
+          product_name: 'Rice',
+        },
+        {
+          id: 3,
+          product_name: 'Eggs',
+        },
+      ],
+      delivery: [
+        {
+          id: 1,
+          delivery_date: '2025-11-09',
+          delivery_product: 'Rice',
+          delivery_product_amount: 12,
+        },
+        {
+          id: 2,
+          delivery_date: '2025-11-15',
+          delivery_product: 'Tea',
+          delivery_product_amount: 200,
+        },
+      ],
+    },
+    {
+      id: 2,
+      supplier_name: 'Shree Krishna',
+      supplier_products: [
+        {
+          id: 1,
+          product_name: 'Daal',
+        },
+        {
+          id: 2,
+          product_name: 'Pea',
+        },
+        {
+          id: 3,
+          product_name: 'Spices',
+        },
+      ],
+      delivery: [
+        {
+          id: 1,
+          delivery_date: '2025-11-09',
+          delivery_product: 'Spices',
+          delivery_product_amount: 200,
+        },
+        {
+          id: 2,
+          delivery_date: '2025-11-15',
+          delivery_product: 'Daal',
+          delivery_product_amount: 10,
+        },
+      ],
+    },
+    {
+      id: 2,
+      supplier_name: 'Shree Krishna',
+      supplier_products: [
+        {
+          id: 1,
+          product_name: 'Daal',
+        },
+        {
+          id: 2,
+          product_name: 'Pea',
+        },
+        {
+          id: 3,
+          product_name: 'Spices',
+        },
+        {
+          id: 4,
+          product_name: 'Chips',
+        },
+      ],
+      delivery: [
+        {
+          id: 1,
+          delivery_date: '2025-11-09',
+          delivery_product: 'Spices',
+          delivery_product_amount: 200,
+        },
+        {
+          id: 2,
+          delivery_date: '2025-11-15',
+          delivery_product: 'Daal',
+          delivery_product_amount: 10,
+        },
+      ],
+    },
+  ];
+
+  getTotalWeeks(): Array<number> {
+    let weeks = [];
+    for (let i = 1; i <= this.totalWeeks; i++) {
+      weeks.push(i);
+    }
+    return weeks;
+  }
+
+  onMonthChange(event: any) {
+    const [year, month] = event.target.value.split('-');
+    const formatted = year + '-' + (month - 1).toString().padStart(2, '0');
+    this.currentMonth.next(formatted.toString());
+  }
+
+  onWeekChange(event: any) {
+    let value = event.target.value == 'null' ? null : event.target.value;
+    this.selectedWeek$.next(value);
+  }
 }
